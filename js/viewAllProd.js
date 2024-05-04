22c2109679a5e3b6e1cd5cac437360fe3b36e77b
sirocco39 <135220339+sirocco39@users.noreply.github.com>
2024-05-04 10:29:05 +0700

    Update viewAllProd.js

--- js/viewAllProd.js
+++ js/viewAllProd.js
@@ -4,24 +4,19 @@ let o=0
 
 // console.log(trigger);
 
-trigger.onclick = () =>{
-    // alert("tes");
-    if(o==1){
-        menu.classList.remove('active');
-        o=0;
-    }else{
-        menu.classList.add('active');
-        o=1;
-    }
-}
-
 function dropdown(trigger, menu){
-    trigger.onmouseover = () =>{
-        menu.classList.add('active');
+    let open = false;
+    trigger.onclick = () =>{
+        if(open == false){
+            menu.style.display = 'block';
+            open = true;
+        }
+        else{
+            menu.style.display = 'none';
+            open = false;
+        }
     }
-    trigger.onmouseout = () => {
-        menu.classList.remove('active');
-    }
+
 }
 
 let elecTrigger = document.querySelector('#elecs');
@@ -40,7 +35,27 @@ let sportTrigger = document.querySelector('#sportTrig');
 let sportMenu = document.querySelector('#sportMenu');
 dropdown(sportTrigger, sportMenu);
 
+trigger.onclick = () =>{
+    // alert("tes");
+    if(o==1){
+        menu.classList.remove('active');
+        elecMenu.style.display = 'none';
+        homeMenu.style.display = 'none';
+        autoMenu.style.display = 'none';
+        sportMenu.style.display = 'none';
+        o=0;
+    }else{
+        menu.classList.add('active');
+        o=1;
+    }
+}
+
 let bobimg = document.querySelector('#bob-tank');
+let itemImg = document.querySelector('item');
+
+itemImg.onclick = () =>{
+    window.location.href = "viewProdDetail.html";
+}
 
 bobimg.onclick = () =>{
     window.location.href = "viewProdDetail.html";
@@ -50,4 +65,11 @@ let home = document.querySelector('#home');
 
 home.onclick = () => {
     window.location.href = "viewAllProd.html";
+}
+
+let seemore = document.querySelector('#seemore');
+
+seemore.onclick = () => {
+    let hidden = document.querySelector('#hidden');
+    hidden.style.display = 'flex';
 }
